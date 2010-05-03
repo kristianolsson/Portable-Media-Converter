b0705e39b8f25304319a72075ff038386dd156d9
Kristian Olsson <github.com@kasa.nu>
2010-05-03 11:22:58 +0200

    first commit

---
+++ portable_media_converter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR="/media/android/"
+
+for f in `find $DIR \( -iname '*.avi' -o -iname '*.mkv' \)`
+do
+  if [ ! -e "$f.mp4" ]
+  then
+#    echo "$f reday to convert"
+    HandBrakeCLI -i $f -o $f.tmp --preset="iPhone & iPod Touch"
+    mv $f.tmp $f.mp4
+  fi
+done
+
